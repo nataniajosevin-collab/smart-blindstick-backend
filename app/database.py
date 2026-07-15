8357d1654f94051cde3a74953f3d9fdd4f3702e2
Josephine Nathania Budiharja <jose@example.com>
2026-07-15 23:03:00 +0700

    fix encoding password mongo db

--- app/database.py
+++ app/database.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://nataniajosevin_db_user:x4NE!UGYdtfL7%23S@cluster0.o4uxoky.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "smart_blindstick")
-
 client = None
 db = None
 
